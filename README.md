276a757daaae25eb1a61e19700f7d4f136ca918c
Patricio-Andre <a260363@dac.unicamp.br>
2026-01-11 03:01:09 -0300

    Interrupção setada para o Tim 2 alternar o estado do led

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# blink by button with interrupt — Nucleo G474RE
+# button and timer interrupts to blink a led — Nucleo G474RE
 
-This repository contains a Rust example that blinks the LED (PA5) when the User Button (PC13) is pressed on the Nucleo G474RE board. In this example, the functionality is implemented using interrupts to transition between states, with precise delay times.
+This repository contains a Rust example demonstrating how to blink an LED (PA5) when the User Button (PC13) is pressed on the Nucleo G474RE board.
+
+The functionality is implemented using external interrupts to handle button presses and timer interrupts to manage precise LED toggling delays.
+
+Note: Currently, the delay between interrupts is limited to a maximum of 1 second due to the specific timer configuration used in this example.
 
 The project is intended as a starting point for
 embedded Rust development on the STM32G4xx family. The repository includes a
@@ -17,8 +21,8 @@ Main contents:
 ## Quick overview
 
 The crate is configured for the `thumbv7em-none-eabihf` target and uses the
-`stm32g474` feature of `stm32g4xx-hal`. This example focuses on blinking the
-LED and does not depend on any logging backend by default.
+`stm32g474` feature of `stm32g4xx-hal`. 
+
 
 ## Prerequisites
 
@@ -110,4 +114,4 @@ with the Cortex-Debug extension. Make sure to install the extension and adjust
 the `executable` path if necessary.
 
 ## References
-This project structure and logic were inspired by the excellent examples from The Embedded Rustacean: [STM32F4 Embedded Rust at the HAL: GPIO Button Controlled Blinking](https://blog.theembeddedrustacean.com/stm32f4-embedded-rust-at-the-hal-gpio-interrupts).
+This project structure and logic were inspired by the excellent examples from The Embedded Rustacean: [STM32F4 Embedded Rust at the HAL: Timer Interrupts](https://blog.theembeddedrustacean.com/stm32f4-embedded-rust-at-the-hal-timer-interrupts).
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-//! Minimal example: blink the LED on the Nucleo G474RE board.
+//! example: blink the LED on the Nucleo G474RE board.
 //!
-//! This binary is built with `no_std` and `cortex-m-rt` and demonstrates a
-//! simple loop that toggles a GPIO pin to turn an LED on and off.
-//! Provides information via Real Time Transfer (RTT) logs 
+//! This binary is built with `no_std` and `cortex-m-rt` and demonstrates an
+//! application with interrupts driven by both GPIO (button) and a hardware timer (TIM2).
+//! It provides information via Real Time Transfer (RTT) logs.
 //! Inline comments provide guidance for learning and documentation.
 
 // Deny warnings and unsafe code to simplify teaching and testing.
@@ -16,15 +16,21 @@
 // Import convenience traits for configuring pins and clocks.
 use hal::prelude::*;
 // Access device peripheral structures from the HAL.
+// stm32 deppends on what board do you use.
 use hal::stm32;
-use stm32g4xx_hal::gpio::ExtiPin;
-use stm32g4xx_hal::gpio::Floating;
-use stm32g4xx_hal::gpio::Input;
-use stm32g4xx_hal::gpio::gpioc;
+use hal::gpio::{ExtiPin,
+                Floating,
+                PushPull,
+                Input,
+                Output,
+                gpioc,
+                gpioa};
+
 
 // Example HAL structure
 
-use stm32g4xx_hal::syscfg::SysCfgExt;
+use hal::syscfg::SysCfgExt;
+
 // Alias the HAL crate for consistent usage in the code.
 use stm32g4xx_hal as hal;
 
@@ -39,22 +45,39 @@ use defmt;
 use defmt_rtt as _;
 
 // Configuring interrupts
+use hal::stm32::TIM2;
 
 use core::cell::{Cell, RefCell};
 
 use cortex_m::interrupt::Mutex;
 
-use stm32g4xx_hal::gpio::SignalEdge as SignalEdge;
+use hal::gpio::SignalEdge as SignalEdge;
 
-use stm32g4xx_hal::interrupt;
+use hal::interrupt;
 
-// Alias for pin button
+// Configuring Timer
+
+use hal::timer::{Timer,
+                 Event,
+                 CountDownTimer};
+
+// Alias for button pin
 type ButtonPin = gpioc::PC13<Input<Floating>>;
 
-// Setting Mutex for interupts
+// Alias for led pin
+type LedPin = gpioa::PA5<Output<PushPull>>;
+
+
+// Setting Mutex for interrupts
+// Create a Global Variable for the Button GPIO Peripheral that I'm going to pass around.
 static G_BUTTON: Mutex<RefCell<Option<ButtonPin>>> = Mutex::new(RefCell::new(None));
-// Setting Mutex for Delay, initial Value 2 seconds
-static G_DELAYMS: Mutex<Cell<u32>> = Mutex::new(Cell::new(2000_u32));
+// Create a Global Variable for the Timer Peripheral that I'm going to pass around.
+static G_TIM: Mutex<RefCell<Option<CountDownTimer<TIM2>>>> = Mutex::new(RefCell::new(None));
+// Create a Global Variable for the LED GPIO Peripheral that I'm going to pass around.
+static G_LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
+// Create a Global Variable for the delay value that I'm going to use to manage the delay.
+static G_DELAYMS: Mutex<Cell<u32>> = Mutex::new(Cell::new(1000));
+
 
 // Minimal panic handler for `no_std` embedded programs.
 #[panic_handler]
@@ -71,8 +94,6 @@ fn main() -> ! {
     // `take()` returns `Some(Peripherals)` only once; it will fail if
     // peripherals have already been taken elsewhere.
     let mut dp = stm32::Peripherals::take().expect("cannot take peripherals");
-    // taking core peripherals necessary to SYST
-    let cp = stm32::CorePeripherals::take().unwrap();
     // Build the Reset & Clock Control (RCC) configuration.
     let mut rcc = dp.RCC.constrain();
     // Hardware initialization.
@@ -81,13 +102,27 @@ fn main() -> ! {
     let gpioc = dp.GPIOC.split(&mut rcc);
     // Setting clocks
     // consumes the configuration!
-    let clocks = rcc.clocks;
-    let mut delay = cp.SYST.delay(&clocks);
-    // Configure Button Pin for Interrupts
+    // uses hsi = 16 mhz
+    let config = hal::rcc::Config::default();
+    let rcc = rcc.freeze(config);
+    let timer = Timer::new(dp.TIM2, &rcc.clocks);
+
+    // Turn it into a CountDownTimer.
+    // Note: 1000ms is roughly the maximum duration settable using this simple method
+    // due to integer math limitations when converting ms to Hz (frequency = 1/period).
+    // To achieve longer time spans, you should use `fugit` types or manual prescalers.
+
+    let mut count_down_timer = timer.start_count_down(1000.ms());
+    
+    // starts watching timeouts to trigger interrupts
+    count_down_timer.listen(Event::TimeOut);
+
+
+   // Configure Button Pin for Interrupts
     
     // Configure PA5 as push-pull output — LED pin on Nucleo boards.
-    let mut led = gpioa.pa5.into_push_pull_output();
-    // Configure PC13 as input. No need to me mutable, we're only reading it.
+    let led = gpioa.pa5.into_push_pull_output();
+    // Configure PC13 as input. No need to be mutable, we're only reading it.
     let mut button = gpioc.pc13.into_floating_input();
     
     
@@ -99,27 +134,26 @@ fn main() -> ! {
     button.trigger_on_edge(&mut dp.EXTI, SignalEdge::Rising);
     // 4) Enable gpio interrupt for button
     button.enable_interrupt(&mut dp.EXTI);
-    // First unsafe code 🥳
+
     // Enable the external interrupt in the NVIC by passing the button interrupt number
     unsafe {
-        cortex_m::peripheral::NVIC::unmask(stm32g4xx_hal::interrupt::EXTI15_10);
+        cortex_m::peripheral::NVIC::unmask(interrupt::EXTI15_10);
+        cortex_m::peripheral::NVIC::unmask(interrupt::TIM2);
     }
+
     // Now that button is configured, move button into global context
+    // Define critical section for button, led and the timer that you choose
     cortex_m::interrupt::free(|cs| {
         G_BUTTON.borrow(cs).replace(Some(button));
-    });
-
-    // Checking the initial value of the Delay using mutex to access a restrict area avoiding concurrency
-    cortex_m::interrupt::free(|cs| {
-        defmt::info!("Delay Initial Value: {} ms", G_DELAYMS.borrow(cs).get());
+        G_LED.borrow(cs).replace(Some(led));
+        G_TIM.borrow(cs).replace(Some(count_down_timer));
+        defmt::info!("Delay Atual: {} ms", G_DELAYMS.borrow(cs).get());
     });
 
     loop {
-        led.set_high().unwrap();
-        delay.delay_ms(cortex_m::interrupt::free(|cs| G_DELAYMS.borrow(cs).get()));
-
-        led.set_low().unwrap();
-        delay.delay_ms(cortex_m::interrupt::free(|cs| G_DELAYMS.borrow(cs).get()));
+        // wfi stands for wait for interrupt and what it does is send the processor to sleep while it's sitting idle
+        // Comment this line to use info! or other defmt macros
+        cortex_m::asm::wfi();
     }
 }
 
@@ -132,12 +166,39 @@ fn EXTI15_10() {
         G_DELAYMS
             .borrow(cs)
             .set(G_DELAYMS.borrow(cs).get()/2);
+
         if G_DELAYMS.borrow(cs).get() < 125_u32 {
-            G_DELAYMS.borrow(cs).set(2000_u32);
+            G_DELAYMS.borrow(cs).set(1000_u32);
         }
+
+        let mut timer = G_TIM.borrow(cs).borrow_mut();
         defmt::info!("Delay Atual: {} ms", G_DELAYMS.borrow(cs).get());
+        timer
+            .as_mut()
+            .unwrap()
+            .start(G_DELAYMS.borrow(cs).get().ms());
+
         // Obtain access to Global Button Peripheral and Clear Interrupt Pending Flag
         let mut button = G_BUTTON.borrow(cs).borrow_mut();
         button.as_mut().unwrap().clear_interrupt_pending_bit();
+    });
+}
+
+// Timer Interrupt
+#[interrupt]
+fn TIM2() {
+    // When Timer Interrupt Happens Two Things Need to be Done
+    // 1) Toggle the LED
+    // 2) Clear Timer Pending Interrupt
+
+    // Start a Critical Section
+    cortex_m::interrupt::free(|cs| {
+        // Obtain Access to Delay Global Data and Adjust Delay
+        let mut led = G_LED.borrow(cs).borrow_mut();
+        led.as_mut().unwrap().toggle().ok();
+
+        // Obtain access to Global Timer Peripheral and Clear Interrupt Pending Flag
+        let mut timer = G_TIM.borrow(cs).borrow_mut();
+        timer.as_mut().unwrap().clear_interrupt(Event::TimeOut);
     });
 }
